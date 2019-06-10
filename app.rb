aaeca73b5c1cf3542d53c71edb22606ccec8c056
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-10 16:31:53 +0100

    adds several new routes

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  "Or will I?"
+end
+
+get '/bubble' do
+  "Bubble!!!!!"
+end
+
+get '/makers' do
+  "That's where you are"
+end
+
+get '/academy' do
+  "Congratulations"
 end
