5a2acfc1c0de4cc83c6fa1f69082556894bf2144
joseph townshend <joetownshend@mac.com>
2019-06-10 17:39:51 +0100

    random-cat James

--- app.rb
+++ app.rb
@@ -22,7 +22,13 @@ get '/academy' do
   "Congratulations"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
